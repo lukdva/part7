d807a44cf625f419e574e5e824d5f27762f8d334
LukasD <lukas.dvarionis@gmail.com>
2022-09-14 17:30:45 +0300

    Changed prettier ignore path for vscode workspace Removed comments from eslintrc

--- bloglist-frontend/.eslintrc.js
+++ bloglist-frontend/.eslintrc.js
@@ -20,37 +20,12 @@ module.exports = {
     },
     "plugins": [
         "react", "jest", "cypress",
-        //  "prettier"
     ],
     "rules": {
-        // "indent": [
-        //     "error",
-        //     2  
-        // ],
-        // "linebreak-style": [
-        //     "error",
-        //     "unix"
-        // ],
-        // "quotes": [
-        //     "error",
-        //     "single"
-        // ],
-        // "semi": [
-        //     "error",
-        //     "never"
-        // ],
         "eqeqeq": "error",
-        // "no-trailing-spaces": "error",
-        // "object-curly-spacing": [
-        //     "error", "always"
-        // ],
-        // "arrow-spacing": [
-        //     "error", { "before": true, "after": true }
-        // ],
         "no-console": 0,
         "react/prop-types": 0,
-        "react/react-in-jsx-scope": "off",
-        // "prettier/prettier": "error"
+        "react/react-in-jsx-scope": "off"
     },
     "settings": {
       "react": {
