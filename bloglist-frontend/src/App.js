5ef4e313b77e4eb6a0536c7571edc3bd5acdd784
LukasD <lukas.dvarionis@gmail.com>
2022-09-18 21:24:23 +0300

    7.15: Individual user view Added Individual user view component (User) Moved users initiliazation from Users component to App

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -8,27 +8,43 @@ import Notification from './components/Notification'
 import Togglable from './components/Togglable'
 import React from 'react'
 import Users from './components/Users'
+import User from './components/User'
 import { useSelector, useDispatch } from 'react-redux'
 import { setUser } from './reducers/LoggedInUserReducer'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useMatch } from 'react-router-dom'
+import { initializeUsers } from './reducers/UsersReducer'
 
 const App = () => {
-  const user = useSelector((state) => state.loggedInUser)
+  const signedUser = useSelector((state) => state.loggedInUser)
   const dispatch = useDispatch()
-  console.log('user', user)
+  // console.log('user', user)
   const blogFormRef = useRef()
+  const users = useSelector((state) => state.users)
 
   useEffect(() => {
     const loggedInUser = window.localStorage.getItem('loggedInUser')
     if (loggedInUser) {
       const userObj = JSON.parse(loggedInUser)
       dispatch(setUser(userObj))
+      dispatch(initializeUsers())
     }
   }, [])
-
+  const userMatch = useMatch('/users/:id')
+  if (userMatch) {
+    console.log('Number(userMatch.params.id)', Number(userMatch.params.id))
+    console.log('match', userMatch)
+  }
+  const user = userMatch
+    ? users.find((user) => {
+        // console.log('user.id', user.id)
+        // console.log('Number(userMatch.params.id)', Number(userMatch.params.id))
+        return user.id === userMatch.params.id
+      })
+    : null
+  console.log(user)
   return (
     <>
-      {user === null && (
+      {signedUser === null && (
         <>
           <Title name="log in to application" />
           <Notification />
@@ -36,11 +52,11 @@ const App = () => {
         </>
       )}
 
-      {user !== null && (
+      {signedUser !== null && (
         <>
           <Title name="blogs" />
           <Notification />
-          <UserInfo user={user} />
+          <UserInfo user={signedUser} />
           <Routes>
             <Route
               path="/"
@@ -50,11 +66,12 @@ const App = () => {
                   <Togglable buttonText="new note" ref={blogFormRef}>
                     <BlogForm blogFormRef={blogFormRef} />
                   </Togglable>
-                  <BlogList user={user} />
+                  <BlogList user={signedUser} />
                 </>
               }
             />
             <Route path="/users" element={<Users />} />
+            <Route path="/users/:id" element={<User user={user} />} />
           </Routes>
         </>
       )}
--- bloglist-frontend/src/components/Users.js
+++ bloglist-frontend/src/components/Users.js
@@ -1,17 +1,7 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { initializeUsers } from '../reducers/UsersReducer'
+import { Link } from 'react-router-dom'
 
-const Users = () => {
-  const users = useSelector((state) => state.users)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(initializeUsers())
-  }, [])
-
+const Users = ({ users }) => {
   return (
     <>
       <h2>Users</h2>
@@ -25,7 +15,9 @@ const Users = () => {
         <tbody>
           {users.map((user) => (
             <tr key={user.id}>
-              <td>{user.name}</td>
+              <td>
+                <Link to={`/users/${user.id}`}>{user.name}</Link>
+              </td>
               <td>{user.blogs.length}</td>
             </tr>
           ))}
